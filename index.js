3ca33e0d7ec43dce684f7b6b65768166d738684a
SAREEMPRO <Mdsareem1@gmail.com>
2023-12-27 22:36:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let moon = document.getElementById("moon");
+let text = document.getElementById("text");
+let train = document.getElementById("train");
+let desert_moon = document.getElementById("desert-moon");
+let man = document.getElementById("man");
+
+window.addEventListener("scroll",()=>{
+let value = window.scrollY;
+moon.style.top = value * .7 + "px";
+text.style.top = 80 + value * -0.2 + '%';
+train.style.left = value * 1.5 + "px"; 
+desert_moon.style.top = value * .3 + "px";
+man.style.left = value * .7 + "px"; 
+
+
+})
+  document.addEventListener('DOMContentLoaded', function () {
+            const menuToggle = document.querySelector('.menu-toggle');
+            const navList = document.querySelector('.navlist');
+            const body = document.body;
+
+            menuToggle.addEventListener('click', function () {
+                navList.classList.toggle('show');
+                body.classList.toggle('menu-active');
+            });
+
+            // Close the menu when a navigation link is clicked
+            navList.querySelectorAll('a').forEach(function (link) {
+                link.addEventListener('click', function () {
+                    navList.classList.remove('show');
+                    body.classList.remove('menu-active');
+                });
+            });
+        });
